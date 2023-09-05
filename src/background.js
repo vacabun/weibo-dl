bbef22c9716e9deed7fcb2d5b8cd8550d1c6fdf8
vacabun <maguotong66@gmail.com>
2023-09-05 22:08:46 +0800

    update 1.0.2

--- src/background.js
+++ src/background.js
@@ -4,9 +4,16 @@ chrome.runtime.onMessage.addListener(
             "from a content script:" + sender.tab.url :
             "from the extension");
         if (request.type === "download_video") {
-            chrome.downloads.download({
-                url: request.url,
-                filename: request.filename
+            let newTabId;
+            chrome.tabs.create({url: request.url}, function(tab) {
+                newTabId = tab.id;
+            });
+            chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+                if (tabId === newTabId && changeInfo.status === 'complete') {
+                    chrome.downloads.download({url: request.url, filename: request.filename}, function() {
+                        chrome.tabs.remove(tabId);
+                    });
+                }
             });
             sendResponse("ok");
         }
